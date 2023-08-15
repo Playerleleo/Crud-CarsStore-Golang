bf9e6727ed69b205e268aabc3fbde4c3efa61d0f
Playerleleo <leonardogomesdossantos123@gmail.com>
2023-08-15 13:42:28 -0300

    metodo Post

--- main.go
+++ main.go
@@ -5,25 +5,34 @@ import (
 )
 
 type Car struct {
-	Nome  string
+	Name  string
 	Price float64
 }
 
 var cars []Car
 
-func createCars() {
-	cars = append(cars, Car{Nome: "Corsa", Price: 1000})
-	cars = append(cars, Car{Nome: "Celta", Price: 2000})
-	cars = append(cars, Car{Nome: "Sivic", Price: 10000})
+func generateCars() {
+	cars = append(cars, Car{Name: "Corsa", Price: 1000})
+	cars = append(cars, Car{Name: "Celta", Price: 2000})
+	cars = append(cars, Car{Name: "Sivic", Price: 10000})
 }
 
 func main() {
-	createCars()
 	e := echo.New()
 	e.GET("/cars", getCars)
+	e.POST("/cars", createCars)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func getCars(c echo.Context) error {
 	return c.JSON(200, cars)
+}
+
+func createCars(c echo.Context) error {
+	car := new(Car)
+	if err := c.Bind(car); err != nil {
+		return err
+	}
+	cars = append(cars, *car)
+	return c.JSON(200, car)
 }
